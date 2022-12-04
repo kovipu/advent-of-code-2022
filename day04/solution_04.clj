4d12b56da638397e9246587cb2b94ef15976bb99
Konsta Purtsi <konsta@arado.fi>
2022-12-04 20:55:11 +0200

    Solve day 4 part 2.

--- day04/solution_04.clj
+++ day04/solution_04.clj
@@ -24,10 +24,22 @@
        (filter identity)
        count))
 
+(defn is-overlapping2 [[a1, a2, b1, b2]]
+  (or
+   (and (<= a1 b1) (<= b1 a2))
+   (and (<= a1 b2) (<= b2 a2))
+   (and (<= b1 a1) (<= a1 b2))
+   (and (<= b1 a2) (<= a2 b2))))
+
 (defn part-2
   "day 04 part 2"
   [input]
-  0)
+  (->> input
+       str/split-lines
+       (map parse-line)
+       (map is-overlapping2)
+       (filter identity)
+       count))
 
 (comment
   (def test-input "2-4,6-8
@@ -38,4 +50,8 @@
 2-6,4-8")
 
   (def output (part-1 test-input))
-  (= output 2))
+  (= output 2)
+
+  (def output2 (part-2 test-input))
+  output2
+  (= output2 4))
