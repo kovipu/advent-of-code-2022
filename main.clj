eb31ad3df09538295e02e9a3d9e1a1c3e32a0400
Konsta Purtsi <konsta@arado.fi>
2022-12-04 16:36:30 +0200

    Adds base for running inputs.

--- main.clj
+++ main.clj
@@ -1,5 +1,10 @@
-(ns main)
+(ns main (:require [day01.solution-01 :as d01]))
 
+(defn- read-input
+  [day]
+  (slurp (clojure.java.io/resource day)))
+  
 (defn -main
+  "Main function called from the command line."
   [& args]
-  (println "Hello world!"))
+  (println (d01/part-1 (read-input "day01/input_01.txt"))))
