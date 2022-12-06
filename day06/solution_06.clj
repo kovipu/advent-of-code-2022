155d8de9ec774dcff6e5055a48a13c880a307051
Konsta Purtsi <konsta@arado.fi>
2022-12-06 22:35:34 +0200

    Solve day 6 part 2.

--- day06/solution_06.clj
+++ day06/solution_06.clj
@@ -1,15 +1,18 @@
-(ns day06.solution-06
-  (:require [clojure.string :as str] [clojure.pprint :as pprint] ))
+(ns day06.solution-06)
 
 (defn find-index [f coll]
   (count (take-while (complement f) coll)))
 
-(defn find-marker [line]
-  (let [first-char (first line)
-        line2 (str first-char line "..")
-        line3 (str (apply str (repeat 2 first-char)) line ".")
-        line4 (str (apply str (repeat 3 first-char)) line)
-        zipped (map vector line line2 line3 line4)]
+(defn gen-lines [line num-lines]
+  (let [first-char (first line)]
+    (map
+     ;; repeat the first character n times.
+     #(str (apply str (repeat % first-char)) line)
+     (range num-lines))))
+
+(defn find-marker [line num-lines]
+  (let [lines (gen-lines line num-lines)
+        zipped (apply map vector lines)]
     (->> zipped
          (find-index #(apply distinct? %))
          inc)))
@@ -17,19 +20,20 @@
 (defn part-1
   "Day 06 part 1"
   [input]
-  (find-marker input))
+  (find-marker input 4))
 
 (defn part-2
   "Day 06 part 2"
   [input]
-  -1)
+  (find-marker input 14))
 
 (comment
   (def test-line "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-  (def test-line-output (find-marker test-line))
+  (def test-line-output (find-marker test-line 4))
   (= test-line-output 7)
-  
+
   (def test2 "bvwbjplbgvbhsrlpgdmjqwftvncz")
-  (def test2-output (find-marker test2))
+  (def test2-output (find-marker test2 4))
   (= test2-output 5)
-)
+
+  (apply str (repeat 0 ".")))
