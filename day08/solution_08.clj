1e52c89179442428b22073c6f5b2140227350132
Konsta Purtsi <konsta@arado.fi>
2022-12-08 22:21:59 +0200

    Solve day 8 part 2. :)

--- day08/solution_08.clj
+++ day08/solution_08.clj
@@ -1,6 +1,8 @@
 (ns day08.solution-08
   (:require [clojure.string :as str]))
 
+
+;; -- Common helpers -- ;;
 (defn parse-input
   [input]
   (->> input
@@ -8,23 +10,33 @@
        (map #(->> (str/split % #"")))
        (map (fn [line] (map #(Integer/parseInt %) line)))))
 
-(defn is-visible-col [col value]
+(defn get-trees
+  [grid y x]
+  (let [up (->> grid
+                (take y)
+                (map #(nth % x))
+                reverse)
+        down (->> grid
+                  (drop (inc y))
+                  (map #(nth % x)))
+        left (->> (nth grid y)
+                  (take x)
+                  reverse)
+        right (->> (nth grid y)
+                   (drop (inc x)))]
+    [up down left right]))
+
+
+;; -- Part 1 -- ;;
+(defn is-visible-col
+  [col value]
   (->> col
        (every? #(< % value))))
 
 (defn is-visible
   [grid y x]
-  (let [value (-> grid (nth y) (nth x))
-        up (->> grid
-                (take y)
-                (map #(nth % x)))
-        down (->> grid
-                  (drop (inc y))
-                  (map #(nth % x)))
-        left (->> (nth grid y)
-                  (take x))
-        right (->> (nth grid y)
-                   (drop (inc x)))]
+  (let [[up down left right] (get-trees grid y x)
+        value (-> grid (nth y) (nth x))]
     (or (is-visible-col up value)
         (is-visible-col down value)
         (is-visible-col left value)
@@ -49,10 +61,36 @@
             (filter identity)
             count))))
 
+
+;; -- Part 2 -- ;;
+(defn scenic-score-col
+  [value coll]
+  (let [visible-trees (->> coll
+                           (take-while #(< % value))
+                           count)]
+    (if (= visible-trees (count coll))
+      visible-trees
+      (inc visible-trees))))
+
+(defn scenic-score
+  [grid y x]
+  (let [[up down left right] (get-trees grid y x)
+        value (-> grid (nth y) (nth x))]
+    (* (scenic-score-col value up)
+       (scenic-score-col value down)
+       (scenic-score-col value left)
+       (scenic-score-col value right))))
+
 (defn part-2
   "Day 08 part 2"
   [input]
-  -1)
+  (let [grid (parse-input input)
+        height (count grid)
+        width (count (first grid))]
+    (apply max
+           (for [y (range 1 (dec height))
+                 x (range 1 (dec width))]
+             (scenic-score grid y x)))))
 
 (comment
   (def test-input "30373
@@ -63,4 +101,8 @@
 
   (def test-output (part-1 test-input))
   test-output
-  (= test-output 21))
+  (= test-output 21)
+
+  (def test-output2 (part-2 test-input))
+  test-output2
+  (= test-output2 8))
