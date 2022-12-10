d679f27a934d0734b14f27df99282c9468fc71f7
Konsta Purtsi <konsta@arado.fi>
2022-12-10 19:00:35 +0200

    Solve day 9 part 2.

--- day09/solution_09.clj
+++ day09/solution_09.clj
@@ -1,12 +1,39 @@
 (ns day09.solution-09
   (:require [clojure.string :as str]))
 
-(defn run-step [acc [direction step]]
+(defn parse [input]
+  (->> input
+       str/split-lines
+       (map #(hash-map
+              :direction (first %)
+              :num-steps (->> %
+                              (drop 2)
+                              (str/join "")
+                              Integer/parseInt)))))
+
+(defn move-tail [head tail]
+  (let [headX (:x head)
+        headY (:y head)
+        tailX (:x tail)
+        tailY (:y tail)
+        tail-lagging (or
+                      (> (Math/abs (- headX tailX)) 1)
+                      (> (Math/abs (- headY tailY)) 1))]
+    (if tail-lagging
+      {:x (cond
+            (< tailX headX) (inc tailX)
+            (> tailX headX) (dec tailX)
+            :else tailX)
+       :y (cond
+            (< tailY headY) (inc tailY)
+            (> tailY headY) (dec tailY)
+            :else tailY)}
+      tail)))
+
+(defn run-step [acc [direction _]]
   (let [[head-coords tail-coords tail-locations] acc
         x (:x head-coords)
         y (:y head-coords)
-        tailX (:x tail-coords)
-        tailY (:y tail-coords)
         newX (case direction
                \L (dec x)
                \R (inc x)
@@ -15,30 +42,18 @@
                \U (inc y)
                \D (dec y)
                y)
-        tail-lagging (or
-                      (> (Math/abs (- newX tailX)) 1)
-                      (> (Math/abs (- newY tailY)) 1))
-        new-tail-coords (if tail-lagging
-                          {:x (cond
-                                (< tailX newX) (inc tailX)
-                                (> tailX newX) (dec tailX)
-                                :else tailX)
-                           :y (cond
-                                (< tailY newY) (inc tailY)
-                                (> tailY newY) (dec tailY)
-                                :else tailY)}
-                          tail-coords)]
+        new-tail (move-tail {:x newX :y newY} tail-coords)]
     ;; move head to direction.
     ;; if tail is more than one behind -> move
     ;; add new tail-location
     [{:x newX
       :y newY}
-     new-tail-coords
-     (conj tail-locations new-tail-coords)]))
+     new-tail
+     (conj tail-locations new-tail)]))
 
 (defn run-instruction [acc instruction]
-  (let [direction (first instruction)
-        steps (->> instruction (drop 2) (str/join "") (Integer/parseInt))]
+  (let [direction (:direction instruction)
+        steps (:num-steps instruction)]
     ;; change head-coords once per step
     ;; change tail-coord accordingly
     ;; add new tail-coord to locations
@@ -50,22 +65,65 @@
 (defn part-1
   "Day 09 part 1"
   [input]
-  ;; head position: head-coords
-  ;; tail position: [head-coords tail-locations]
   (-> (->> input
-           str/split-lines
+           parse
            (reduce
             run-instruction
-            [{:x 0 :y 0}
-             {:x 0 :y 0}
-             #{}]))
+            [{:x 0 :y 0} ;; head position
+             {:x 0 :y 0} ;; tail position
+             #{} ;; unique tail locations´
+             ]))
       (nth 2)
       count))
+
+
+(defn run-step2 [acc direction _]
+  ;; move first one once,
+  ;; check all others if they need to be moved
+  (let [[rope tail-locations] acc
+        head (first rope)
+        x (:x head)
+        y (:y head)
+        newX (case direction
+               \L (dec x)
+               \R (inc x)
+               x)
+        newY (case direction
+               \U (inc y)
+               \D (dec y)
+               y)
+        new-rope (assoc (into [] rope) 0 {:x newX :y newY})
+        tail-moved (reduce
+                    (fn [rope next]
+                      (conj rope (move-tail (last rope) next)))
+                    [(first new-rope)]
+                    (drop 1 new-rope))]
+    [tail-moved (conj tail-locations (last tail-moved))]))
+
+(defn run-instruction2 [acc instruction]
+  (let [direction (:direction instruction)
+        num-steps (:num-steps instruction)]
+    (reduce
+     #(run-step2 %1 direction %2)
+     acc
+     (for [step (range num-steps)] step))))
 
 (defn part-2
   "Day 09 part 2"
   [input]
-  -1)
+  ;; head-position
+  ;; 9 tails in an array
+  ;; unique locations of the last one in a set.
+  (->
+   (->> input
+        parse
+        (reduce
+         run-instruction2
+         [(for [_ (range 10)] {:x 0 :y 0}) ;; rope position
+          #{} ;; unique tail locations
+          ]))
+   last
+   count))
 
 (comment
 
@@ -77,6 +135,25 @@ R 4
 D 1
 L 5
 R 2")
-  
+
   (def test-output (part-1 test-input))
-  (= test-output 13))
+  test-output
+  (= test-output 13)
+
+  (def test-input2 "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20")
+
+  (def test-output2 (part-2 test-input2))
+  test-output2
+  (= test-output2 36)
+  test-output2
+
+  (def rope (lazy-seq [{:x 1 :y 2} {:x 2 :y 5}]))
+  rope
+  (assoc (into [] rope) 0 {:x -1 :y -1}))
---
+++ lib/library.clj
@@ -0,0 +1,20 @@
+(ns lib.library 
+  (:require [clojure.string :as str]))
+
+(defn print-map [width height coords]
+  (let [m (for [y (range (- height) height)]
+            (for [x (range (- width) width)]
+              (let
+               [idx (.indexOf coords {:x x :y y})]
+                (if (= -1 idx)
+                  "."
+                  (str idx)))))]
+    (->> m
+         (map #(str/join "" %))
+         (str/join "\n")
+         println)))
+
+(comment 
+  (def coords [{ :x 1 :y 1} {:x 2 :y 2}])
+  (print-map 5 5 coords)
+  )
