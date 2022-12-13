4dfd3dbdada88093c2258b51be2a996d85fd1a4e
Konsta Purtsi <konsta@arado.fi>
2022-12-13 22:48:45 +0200

    Solve day 13 part 2.

--- day13/solution_13.clj
+++ day13/solution_13.clj
@@ -17,8 +17,8 @@
 (defn is-ordered? [left right]
   (cond
     (and (number? left) (number? right)) (if (= left right)
-                                     :recurse-please
-                                     (< left right))
+                                           :recurse-please
+                                           (< left right))
 
     (number? left) (is-ordered? [left] right)
 
@@ -29,12 +29,11 @@
     (empty? left) true
 
     (empty? right) false
-  
+
     :else (let [pair-ordered? (is-ordered? (first left) (first right))]
             (if (= :recurse-please pair-ordered?)
               (is-ordered? (rest left) (rest right))
-              pair-ordered?)))
-  )
+              pair-ordered?))))
 
 (defn part-1
   "Day 13 part 1"
@@ -47,10 +46,25 @@
                    (+ acc idx 1)
                    acc)) 0)))
 
+(defn parse-2 [input]
+  (->> input
+       str/split-lines
+       (remove empty?)
+       (map edn/read-string)))
+
 (defn part-2
   "Day 13 part 2"
   [input]
-  -1)
+  (let [div1 [[2]]
+        div2 [[6]]]
+    (->> input
+         parse-2
+         (into [div1 div2])
+         (sort is-ordered?)
+         (with-index)
+         (reduce (fn [acc [idx signal]] (if (#{div1 div2} signal)
+                                          (* acc (inc idx))
+                                          acc)) 1))))
 
 (comment
   (def input "[1,1,3,1,1]
@@ -77,4 +91,5 @@
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]")
 
-  (part-1 input))
+  (part-1 input)
+  (part-2 input))
