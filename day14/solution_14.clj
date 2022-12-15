f0dacc2ca3354a4a6995efc1523b606ccfcbb1fa
Konsta Purtsi <konsta@arado.fi>
2022-12-15 20:04:32 +0200

    Solve day 14 part 2.

--- day14/solution_14.clj
+++ day14/solution_14.clj
@@ -1,11 +1,5 @@
 (ns day14.solution-14
-  (:require [clojure.string :as str]
-            [lib.library :refer [print-map]]))
-
-(comment
-  (def line (parse-line "498,4 -> 498,6 -> 496,6"))
-  line
-  (reduce add-rockline #{} line))
+  (:require [clojure.string :as str]))
 
 (defn parse-pair [pair]
   (let [[x y] (str/split pair #",")]
@@ -22,8 +16,6 @@
   (->> input
        str/split-lines
        (map parse-line)))
-
-
 
 (defn add-rockline [rockmap [start end]]
   (let [[x1 x2] (sort [(:x start) (:x end)])
@@ -90,13 +82,52 @@
         sand-filled (drop-sand rockmap bottom 500 0)]
     (- (count sand-filled) (count rockmap))))
 
+(defn drop-sand-2 [state x y]
+  (loop [state state
+         x x
+         y y]
+    ;; drop a sand from sand-coords
+    ;; drop another sand when it lands.
+    ;; stop when full
+    (cond
+      ;; empty space right under?
+      (not (state {:x x :y (inc y)})) (recur state x (inc y))
+
+      ;; empty space down and to the left?
+      (not (state {:x (dec x) :y (inc y)})) (recur state (dec x) (inc y))
+
+      ;; empty space down and to the right?
+      (not (state {:x (inc x) :y (inc y)})) (recur state (inc x) (inc y))
+
+      ;; sand comes to rest
+      :else
+      (if (= y 0)
+        ;; we are full -> return state
+        (conj state {:x x :y y})
+        ;; not full -> drop another sand
+        (recur (conj state {:x x :y y}) 500 0)))))
+
 (defn part-2
   "Day 14 part 2"
   [input]
-  -1)
+  (let [rockmap (->> input
+                     parse
+                     (reduce form-rocks #{}))
+        bottom (+ (find-bottom rockmap) 2)
+        ;; add the floor
+        rockmap (add-rockline
+                 rockmap
+                 [{:x -1
+                   :y bottom}
+                  {:x 1001
+                   :y bottom}])
+        
+        sand-filled (drop-sand-2 rockmap 500 0)]
+    (- (count sand-filled) (count rockmap))))
 
 (comment
   (def inp "498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9")
 
-  (part-1 inp))
+  (part-1 inp)
+  (part-2 inp))
